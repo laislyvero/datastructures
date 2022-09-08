979c6d2ab7361c5573ae64ba471c53b31f1a27d5
laislyvero <veronicaafwayi54@gmail.com>
2022-09-08 15:35:10 +0300

    datastructures

---
+++ data.js
@@ -0,0 +1,46 @@
+class Stack {
+    constructor() {
+       this.items = [];
+    }
+    push(element) {
+         this.items.push(element);
+    }
+    pop() {
+        return this.items.pop();
+    }
+    peek() {
+        return this.items[this.items.length -1];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    size() {
+     return this.items.length;  
+    }
+    clear() {
+      return  this.items = [];
+    }
+  } 
+  const data = new Stack();
+  data.push(23);
+  data.push(34);
+  data.push(89);
+  console.log(data);
+
+  data.pop();
+  console.log(data.pop());
+
+  data.peek();
+  console.log(data.peek());
+
+  data.isEmpty();
+  console.log(data.isEmpty());
+
+  data.size();
+  console.log(data.size());
+
+  data.clear();
+  console.log(data.clear());
+
+
+
